da88fba6d005048fdc755c8f7c04e8016990acee
Andrei Sabalenka <mechakotik@gmail.com>
2026-03-31 14:28:28 +0300

    wpe-compile: new codegen architecture

---
+++ wpe-compile/module/main.c
@@ -0,0 +1,5 @@
+#include "openwallpaper.h"
+
+__attribute__((export_name("init"))) void init() {}
+
+__attribute__((export_name("update"))) void update(float delta) {}
---
+++ wpe-compile/module/openwallpaper.h
@@ -0,0 +1,1 @@
+../../include/openwallpaper.h
